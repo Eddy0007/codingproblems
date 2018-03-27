c34d277974bea4dd6e595d37d9631191ff89789a
Eddy <eddy@192.168.1.4>
2018-03-26 23:07:37 -0400

    Refactored first solution to cater for more edge cases check comment before 2nd function

--- kebabizeFunction.js
+++ kebabizeFunction.js
@@ -31,3 +31,30 @@ function kebabize(str) {
     }
     return str;
   }
+
+//   First solution doesn't pass when the input had a number as first char
+//   and Uppercase as 2nd. Also doesn't passed if first char is Uppercase
+function kebabize2(str) {
+    up = {
+    "A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8,"I":9, 
+    "J":10, "K":11, "L":12, "M":13, "N":14, "O":15, "P":16, "Q":17,
+    "R":18, "S":19, "T":20, "U":21, "V":22, "W":23, "X":24,"Y":25, 
+    "Z":26
+    }
+    for(var i=0; i<str.length; i++){
+        if(!isNaN(parseInt(str.charAt(i))) || !isNaN(parseInt(str.charAt(i))) && i==0){
+        str = str.substring(0,i)+str.substring(i+1);
+        i--;
+        }else{
+        if(str.charAt(i) in up){
+            var toLower = str.charAt(i).toLowerCase()
+            if(i !== 0){
+            str = str.substring(0,i)+"-"+toLower+str.substring(i+1);
+            }else{
+            str = str.substring(0,i)+toLower+str.substring(i+1);
+            }
+        }
+        }
+    }
+    return str;
+}
