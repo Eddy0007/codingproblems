c65fa18c6ea72919e477969432d570927f51d969
Eddy <seddy0007@gmail.com>
2018-06-02 19:00:04 -0400

    Does number look big?

---
+++ numberLookBig.js
@@ -0,0 +1,31 @@
+/*
+A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of digits.
+
+For example, take 153 (3 digits):
+
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+and 1634 (4 digits):
+
+    1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+The Challenge:
+
+Your code must return true or false depending upon whether the given number is a Narcissistic number.
+
+Error checking for text strings or other invalid inputs is not required, only valid integers will be passed into the function.
+*/
+
+function narcissistic( value ) {
+    // Code me
+    const valStr = String(value);
+    const arr = valStr.split("");
+    const arr2 = arr.map( (x) => Math.pow(x, arr.length));
+    const addArr2 = y => {
+      let total = 0;
+      for( x of y){
+        total += x;
+      }
+      return total;
+    }
+    return addArr2(arr2)===value ? true : false;
+  }
+  
